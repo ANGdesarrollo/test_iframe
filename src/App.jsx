1fa13ae8c20dec6b4de4b0979a56f9ae5d00321a
Alexis Graff <alexisgraff123@gmail.com>
2025-10-17 11:17:05 -0300

    Feat: add iframe

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <iframe style={{width: '100vw', height: '100vh', border: 'none'}} src="https://devcotizador.cebrokers.com.ar/embed/test_iframe/add4f481-62ab-4858-b541-6f6a0872a15f" frameBorder="0" allowFullScreen></iframe>
     </>
   )
 }
